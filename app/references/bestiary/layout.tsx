9d5e84fd76d20f49d847ba51a6d7246d3f18489e
Tomer Norman <spidernor38@gmail.com>
2025-01-24 12:47:01 +0200

    added suspense to bestiary page

--- app/references/bestiary/layout.tsx
+++ app/references/bestiary/layout.tsx
@@ -1,3 +1,6 @@
+import { LoaderPinwheelIcon } from "lucide-react";
+import { Suspense } from "react";
+
 interface Props {
   children: React.ReactNode;
 }
@@ -6,7 +9,7 @@ export default async function BestiaryLayout({ children }: Props) {
   return (
     <div className="w-full flex flex-col items-center justify-center gap-4 h-screen overflow-hidden">
       <h1 className="text-4xl font-bold">Bestiary</h1>
-      {children}
+      <Suspense fallback={<LoaderPinwheelIcon className="animate-spin" />}>{children}</Suspense>
     </div>
   );
 }
