09a51533279a8f30b45d3e08ed7c1096550713f2
Tomer Norman <spidernor38@gmail.com>
2025-01-31 18:32:08 +0200

    fixed supabase redirect

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import QueryProvider from "./providers/QueryProvider";
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
-import { headers } from "next/headers";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,17 +24,6 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const headersList = await headers();
-  const pathname = headersList.get("x-matched-path") || "";
-
-  if (pathname !== "/login") {
-    const supabase = await createClient();
-
-    const { data, error } = await supabase.auth.getUser();
-    if (error || !data?.user) {
-      redirect("/login");
-    }
-  }
   return (
     <html lang="en">
       <body className={`dark ${geistSans.variable} ${geistMono.variable} antialiased`}>
