3e5cc705389a0eba1df0a7dee80a2689144d6481
TomerNorman <35326023+HitBox38@users.noreply.github.com>
2025-07-21 13:15:43 +0300

    Update layout.tsx

--- app/references/bestiary/layout.tsx
+++ app/references/bestiary/layout.tsx
@@ -14,7 +14,6 @@ export const metadata: Metadata = {
 export default async function BestiaryLayout({ children }: Props) {
   return (
     <div className="w-full flex flex-col items-center justify-center gap-4 h-screen overflow-hidden">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Bestiary</h1>
       <Suspense fallback={<LoaderPinwheelIcon className="animate-spin" />}>{children}</Suspense>
     </div>
   );
